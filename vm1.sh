6009d8831e86579ca1d05e2d2ef50ac1f0198945
Yeshwanth Sai <38140068+yeshu4@users.noreply.github.com>
2021-02-11 22:19:57 -0800

    Update vm1.sh

--- vm1.sh
+++ vm1.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get install python3
 sudo apt-get install python3-pip
 python3 -m pip install -U pip
-pip3 install azure-storage-blob
+pip3 install azure.storage
 pip3 install configparser
 pip3 install flask
